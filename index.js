8e5c997bb3da689864383847ad4b2a8834caf8f0
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-10-20 20:46:33 -0300

    banco digital

---
+++ index.js
@@ -0,0 +1,10 @@
+const cliente1nome = "Rodrigo";
+const cliente1cpf = "10101010";
+const cliente1agencia = "121222";
+const cliente1saldo = 0;
+
+
+const cliente2nome = "keila";
+const cliente2cpf   = "121222211";
+const cliente2agencia   ="111111111";
+const cliente2saldo = 0;
