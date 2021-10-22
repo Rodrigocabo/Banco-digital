1fc90f630936e76191ea8fe256ca01c346aeae71
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-10-21 22:10:56 -0300

    bytebank

--- index.js
+++ index.js
@@ -24,3 +24,12 @@ const cliente2cpf   = "121222211";
 const contacorrente = new contacorrente();
 contacorrentecarlos.agencia = 10101010;
 contacorrentecarlos.agencia = 1001;
+console.log(contacorrentecarlos.saldo);
+contacorrentecarlos.saldo = 100;
+console.log(contacorrentecarlos.saldo);
+let valorsacado = 200;
+contacorrentecarlos.saldo -= valorsacado;
+
+
+console.log(cliente1);
+console.log(cliente2);
