23c27c4715ce63666d29c07b9a1df4d5d0e5ba72
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-10-21 21:57:08 -0300

    bancodigital

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const cliente1nome = "Rodrigo";
+class cliente{
+    nome;
+    cpf;
+    
+}
+
+class contacorrente{
+    agencia;
+    saldo;
+}
+
+
+
+
+
+const cliente1nome = "carlos";
 const cliente1cpf = "10101010";
-const cliente1agencia = "121222";
-const cliente1saldo = 0;
+
 
 
 const cliente2nome = "keila";
 const cliente2cpf   = "121222211";
-const cliente2agencia   ="111111111";
-const cliente2saldo = 0;
+
+const contacorrente = new contacorrente();
+contacorrentecarlos.agencia = 10101010;
+contacorrentecarlos.agencia = 1001;
